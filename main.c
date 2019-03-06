5375884ac2cb1fb913ab518cae1ba0bb1061314e
Max Base <MaxBaseCode@Gmail.Com>
2019-03-06 19:18:10 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
 
+/**
+*
+* @Name : MongoSample.C
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-03-06
+* @Released under : https://github.com/BaseMax/MongoCSample/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/MongoCSample
+*
+**/
